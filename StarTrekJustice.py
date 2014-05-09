4767d19052c03636e785827c15c6fd815ca12621
Selina Musuta <selinamusuta>
2014-05-09 16:06:17 -0400

    Updated 1 question and exit out of game

--- StarTrekJustice.py
+++ StarTrekJustice.py
@@ -13,11 +13,15 @@ print "\nWho organized a union to establish sick leave and other paid benefits f
 
 print "\n\ta) Quark \n\tb) Rom \n\tc) Captain Sisco"
 
+# a) Quark
+# b) Rom
+# c) Captain Sisco
+
 # if user inputs "a", then print "Rom took a stand against his brother after years of wage theft and emotional abuse"
 # if user inputs "b", then print "then print "You may be confused.  Quark was the owner of that popular bar and tried to union bust his brother
 # if user inputs "c", "Sisco's second job is not the owner of an away station bar....but being an emissary to the Bajorans takes up a lot of his time"
+answer = raw_input("\nYour answer is ")
 
-answer = raw_input("\nYour answer is  ")
 
 if answer != "b":
 	print "\nYou maybe confused. Quark was the owner of the only bar on Deep Space 9 and made several attempts to bust up the union that Rom and his wife Leeta formed. "
@@ -27,12 +31,51 @@ elif answer == "b":
 
 else:
 	print "\nSisco's second job is not the owner of an away station bar...but being an Emissary to the Bajorans takes up alot of his time." #I'm having problems printing this and I understand the reason why but would like to add value to c but not sure how
-	
+
+answer = raw_input("\nNext Question (Y/N)  ")	
 	
 # Section 2.	
 # Ask user "What conflict is often used as an analogy in reference to the continued occupation of the Palestinian territories? "
+# a) Romulan and Vulcan split in the 1300s
+# b) The Borg wherever they go
+# c) The Cardassian occupation of Bajor
+
+# if user inputs "a", then print "Your answer is not close.  But Vulcans and Romulans have been warring cousins"
+# if user inputs "b", then print "The answer is c.  The Borg don't occupy, they assimilate, destroy, and leave for the next lifeform"
+# if user inputs "c", then print  "You are correct."
 
 print "\nWhat conflict is often used as an analogy in reference to the continued occupation of the Palestinian territories? "
 
-# if user in puts "a", then print ""
-# if user in puts "b", then print ""
+print "\n\ta) Romulan and Vulcan split in the 1300s \n\tb) The Borg wherever they go \n\tc) The Cardassian occupation of Bajor"
+
+answer = raw_input("\nYour answer is ")
+
+if answer == "a":
+	print "\nYour answer is not close.  But Vulcans and Romulans have been warring cousins"
+
+elif answer == "b":
+	print "\nThe answer is c.  The Borg don't occupy, they assimilate, destroy, and leave for the next lifeform"
+
+else:
+	print "\nYou are correct."
+
+answer = raw_input("\nNext Question (Y/N)  ")
+
+
+
+# Section 3.
+# Ask user "Which beings' central mission is to assimilate the technology and organic information of lifeforms in their collective being?"
+# A commentary on communism.  
+if answer == "Y":
+	print "\nWhich beings' central mission is to assimilate the technology and organic information of lifeforms in their collective being? "
+
+else:
+	print "\nLive Long and Prosper!"
+# if user inputs "a", then print "Resistance is futile! The Borg 's collectivism put off the '"
+# if user inputs "b", then print ""
+
+# Section 4.
+# Ask user "Which character in Deep Space 9 is only referred to with male pronouns by others? "
+
+
+# Trills
